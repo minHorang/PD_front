bca6a2b873f4354788cbdd72d6b1dda9e2cb4f48
eminseo2003 <eminseo2003@naver.com>
2024-11-23 18:59:32 +0900

    Feat: 메인 페이지 수정

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import contestbanner from '../../img/contestbanner.png';
 import './Main.css';
 
 function Main() {
+  const navigate = useNavigate();
+
+  const handlecontest = () => {
+    navigate('/contest');
+  };
+
   return (
     <div className="maincontainer">
-      <img src={contestbanner} alt="contestbanner" className="contestbanner" />
+      <img
+        src={contestbanner}
+        alt="contestbanner"
+        className="contestbanner"
+        onClick={handlecontest}
+      />
     </div>
   );
 }
